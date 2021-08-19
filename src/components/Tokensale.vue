d7b93c6897d3ac52b136084b3f870cf30204e3b2
super1114 <super1114dev@gmail.com>
2021-08-18 21:51:34 -0400

    contract

--- src/components/Tokensale.vue
+++ src/components/Tokensale.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-100"> 
     
-    <div>
+    <div class="relative">
       <div class="mx-6">
         <div class="bg-yellow-100 rounded-b text-yellow-900 px-4 py-3 shadow-md" role="alert">
           <div class="flex">
@@ -22,21 +22,17 @@
           </div>
         </div>
       </div>
-    
-      <!-- <div class="bg-yellow-100 border border-yellow-400 text-yellow-700 px-4 py-3 rounded relative w-3/6 m-auto mt-3" role="alert" v-bind:class="{ hidden: metamaskInstalled }">
-        <strong class="font-bold">Warning!</strong>
-        <span class="block sm:inline">&nbsp;&nbsp;&nbsp;&nbsp;MetaMask was not installed in your computer.</span>
-        <span class="absolute top-0 bottom-0 right-0 px-4 py-3" @click="metamaskInstalled=true">
-          <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-        </span>
-      </div> -->
-
+      <transition name="fade">
+        <div class="fixed inset-x-0 top-0 m-auto mt-4 flex items-center bg-red-500 text-white text-sm font-bold px-4 py-3 lg:w-2/6 rounded" role="alert" v-if="alertShow">
+          <p>{{alertMsg}}</p>
+        </div>
+      </transition>
       <!-- Metamask and Walletconnect buttons -->
       <div class="text-right p-6 space-x-4">
         <button type="button"
                 class="whitespace-nowrap inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <img class="-ml-0.5 mr-2 h-4 w-4" src="../assets/img/providers/ethereum.svg">
-          <span class="hidden lg:block">Chain ID&nbsp;</span> <strong>{{chainId}}</strong>
+          <span class="lg:block">Chain ID&nbsp;</span> <strong>{{this.networkId}}</strong>
           <svg class="w-5 h-5 ml-2 -mr-1" viewBox="0 0 20 20" fill="currentColor">
             <path fill-rule="evenodd"
                   d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
@@ -47,17 +43,17 @@
         <a v-if="ethereum==null" href="http://www.metamask.io" target="_blank"
                 class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <img class="-ml-0.5 mr-2 h-4 w-4" src="../assets/img/providers/metamask.svg">
-          <span class="hidden lg:block">Install MetaMask</span>
+          <span class="lg:block">Install MetaMask</span>
         </a>
-        <button v-else-if="web3.isInjected==false" type="button" @click="connectMetaMask(1)"
+        <button v-else-if="!account" type="button" @click="connectMetaMask(1)"
                 class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <img class="-ml-0.5 mr-2 h-4 w-4" src="../assets/img/providers/metamask.svg">
-          <span  class="hidden lg:block">Connect with MetaMask</span>
+          <span  class="lg:block">Connect with MetaMask</span>
         </button>
         <button v-else type="button" @click="connectMetaMask(2)"
                 class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <img class="-ml-0.5 mr-2 h-4 w-4" src="../assets/img/providers/metamask.svg">
-          <span  class="hidden lg:block">Account {{this.$store.state.web3.coinbase.substring(0,5)+"..."}}</span>
+          <span  class="lg:block">{{this.account.substring(0,6)+"..."+this.account.substring(this.account.length-4,this.account.length)}}</span>
         </button>
       </div>
     </div>
@@ -72,9 +68,9 @@
 
     <div class='metamask-info'>
       <p>Metamask: {{ web3.isInjected }}</p>
-      <p>Network: {{ web3.networkId }}</p>
-      <p>Account: {{ web3.coinbase }}</p>
-      <p>Balance: {{ web3.balance }}</p>
+      <p>Network: {{ this.networkId }}</p>
+      <p>Account: {{ this.account }}</p>
+      <p>Balance: {{ this.balance }}</p>
     </div>
 
     <section class="text-black py-6 overflow-auto">
@@ -108,34 +104,73 @@
     </section>
 
     <!-- Tokensale address part -->
-    <section class="bg-gradient-to-r from-blue-500 to-purple-600 py-12">
-      <div class="max-w-6xl mx-auto" x-data="{ qr: false }">
-        <label class="block font-medium text-white font-bold text-4xl text-center">Send ETH to This Address - or -
-          connect your wallet</label>
-        <div class="my-12 relative items-center">
-          <input type="text" id="address" readonly value="0x0012901290190190190190"
-                 class="p-4 h-16 sm:text-lg md:text-3xl text-gray-600 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 border-gray-300 rounded-md">
-          <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+    <section class="bg-gradient-to-r from-blue-500 to-purple-600 py-12 lg:px-10">
+      <div v-if="!this.ethereum">
+        <div class="max-w-6xl mx-auto" x-data="{ qr: false }">
+          <label class="block font-medium text-white font-bold text-4xl text-center">Send ETH to This Address - or -
+            connect your wallet</label>
+          <div class="my-12 relative items-center">
+            <input type="text" id="address" readonly v-model="account"
+                  class="p-4 h-16 sm:text-lg md:text-3xl text-gray-600 shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 border-gray-300 rounded-md">
+            <div class="absolute inset-y-0 right-0 flex py-1.5 pr-1.5">
+              <button type="button" class="px-2" @click="copyAddress">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3" />
+                </svg>
+              </button>
+              <div class="relative" v-show="copiedTooltip">
+                <div class="absolute top-0 z-10 w-32 p-2 -mt-4 text-sm leading-tight text-white transform -translate-x-full -translate-y-full bg-blue-500 rounded-lg shadow-lg">
+                  Copied Address
+                </div>
+            </div>
+            </div>
             
           </div>
-        </div>
 
-        <div x-show="qr" class="flex justify-center max-w-6xl bg-white shadow-lg rounded-lg p-8 mx-auto text-center">
-          <qr-code text="0x0012901290190190190190"></qr-code>
+          <div x-show="qr" class="flex justify-center max-w-6xl bg-white shadow-lg rounded-lg p-8 mx-auto text-center">
+            <qr-code text="address"></qr-code>
+          </div>
+        </div>
+        <div class="max-w-6xl mx-auto pt-6 pb-6 overflow-auto text-white">
+          <div class="float-left">
+            Max contribution: 100 ETH
+            <br/>
+            Min contribution: 0.01 ETH
+          </div>
+          <div class="float-right">
+            Gas limit: 200,000 UNITS
+            <br/>
+            Gas price: 6 GWEI
+          </div>
         </div>
       </div>
-
-      <div class="max-w-6xl mx-auto pt-6 pb-6 overflow-auto text-white">
-        <div class="float-left">
-          Max contribution: 100 ETH
-          <br/>
-          Min contribution: 0.01 ETH
+      <div v-else class="m-auto d-flex lg:w-2/6 bg-white p-4 rounded .shadow-2xl">
+        <h2 class="block mb-4 px-6 font-bold text-left text-black">
+          Deposit below to purchase ETHX
+        </h2>
+        <div class="mt-10 flex flex-row justify-between px-6">
+          <h6 class="text-sm">Send</h6>
+          <h6 class="text-sm">Balance:{{this.$store.state.web3.balance}}</h6>
         </div>
-        <div class="float-right">
-          Gas limit: 200,000 UNITS
-          <br/>
-          Gas price: 6 GWEI
+        <div class="mt-2 flex flex-row items-center justify-between mb-6 px-6">
+          <input class="appearance-none font-medium text-2xl py-1 rounded w-full  mb-3 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="0.0" v-model="sendAmount">
+          <img src="../assets/img/icons/icon.png" class="w-6 h-6"/>
+          <label class="ml-2 font-semibold text-xl">ETH</label>
         </div>
+        <div class="mt-10 flex flex-row justify-between px-6">
+          <h6 class="text-sm">Receive</h6>
+          <h6 class="text-sm">Balance:0</h6>
+        </div>
+        <div class="mt-2 flex flex-row items-center justify-between mb-6 px-6">
+          <input class="appearance-none font-medium text-2xl py-1 rounded w-full  mb-3 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="0.0" v-model="recvAmount">
+          <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sc-jeCdPy acRau"><circle cx="12" cy="12" r="10"></circle><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"></path><line x1="12" y1="17" x2="12.01" y2="17"></line></svg>
+          <label class="ml-2 font-semibold text-xl">ETHX</label>
+        </div>          
+        <button class="w-full bg-purple-600 hover:bg-purple-600 text-white font-medium py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button"
+          @click="buyToken"
+        >
+          BUY
+        </button>
       </div>
     </section>
 
@@ -177,15 +212,15 @@
       <div class="max-w-6xl mx-auto">
         <div class="flex mb-2 items-center justify-between">
           <div>
-                      <span
-                          class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-red-600 bg-red-200">
-                        ETHX SOLD
-                      </span>
+            <span
+                class="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-red-600 bg-red-200">
+              ETHX SOLD
+            </span>
           </div>
           <div class="text-right">
-                      <span class="text-xs font-semibold inline-block text-red-600">
-                        30%
-                      </span>
+            <span class="text-xs font-semibold inline-block text-red-600">
+              30%
+            </span>
           </div>
         </div>
         <div class="overflow-hidden h-4 mb-4 text-xs flex rounded bg-gray-50">
@@ -386,6 +421,8 @@
 </template>
 
 <script>
+import Web3 from "web3"
+import PresaleJson from "../../contracts/Presale.json"
 export default {
   name: 'Tokensale',
   computed: {
@@ -399,27 +436,105 @@ export default {
         return this.$store.state.myChainId
       }
     },
+    recvAmount(){
+      return this.sendAmount*200/3;
+    }
   },
   data(){
     return {
+      web3Obj : new Web3(Web3.givenProvider || 'ws://localhost:8546'),
+      sendAmount:0,
        ethereum:window.ethereum,
        selected:null,
-       countTime:new Date('Aug 14, 2021 00:00:00').getTime(),
+       countTime:new Date('Aug 20, 2021 00:00:00').getTime(),
        days:"00",
        hours:"00",
        minutes:"00",
-       seconds:"00"
+       seconds:"00",
+       copiedTooltip:false,
+       contractAddr:"",
+       abi:PresaleJson.abi,
+       alertShow:false,
+       alertMsg:"",
+       networkId:"1",
+       account:"",
+       balance:0
     }
   },
-  props: {
-    copyToClipboard: copyToClipboard,
-  },
+  
   created() {
+    if(window.ethereum){
+      this.web3Obj.eth.getAccounts().then((result)=>{
+        this.account = result[0];
+      })
+      window.ethereum.on('networkChanged', (networkId)=>{
+        this.networkId = networkId;
+        if(networkId==1){
+          this.contractAddr="0x76f65b431784ed58aab24fb645b21ee2fcfee7ea";
+        }else if(networkId==56){
+          this.contractAddr="0x0AaFB655636890a1683c1b5cCFAbD12Efd839D09";
+        }else {
+          this.contractAddr="0x76f65b431784ed58aab24fb645b21ee2fcfee7ea";
+        }
+      });
+      window.ethereum.on('accountsChanged', async (accounts) =>{
+        this.account = accounts[0];
+        await this.web3Obj.eth.getBalance(this.account).then((bal)=>{
+          this.balance = bal;
+        });
+        console.log(this.balance);
+      });
+    }
+    
     console.log("created")
     this.startCountDown()
-    this.$store.dispatch("registerWeb3")
+    this.web3Obj.eth.net.getId().then((result)=>{
+      this.networkId = result;
+      if(result==56) this.contractAddr = "0x0AaFB655636890a1683c1b5cCFAbD12Efd839D09";//Bsc
+      else if(result==1) this.contractAddr = "0x76f65b431784ed58aab24fb645b21ee2fcfee7ea";//Ether
+      else this.contractAddr = "0x76f65b431784ed58aab24fb645b21ee2fcfee7ea";//Ether
+    });
   },
   methods:{
+    showAlert:function(msg) {
+      this.alertShow=true;
+      this.alertMsg=msg;
+      setTimeout(()=>{
+        this.alertMsg = "";
+        this.alertShow = false;
+      },1000)
+    },
+    buyToken:function() {
+      console.log(this.networkId);
+      console.log(this.contractAddr);
+      // let balance = this.balance;
+      // if(this.sendAmount<=0){
+      //   return;
+      // }
+      // if(this.sendAmount>balance){
+      //   this.showAlert("Insufficient funds");
+      //   return;
+      // }
+      window.contract = new this.web3Obj.eth.Contract(this.abi, this.contractAddr);
+      console.log(window.contract);
+      window.contract.methods.buy().estimateGas().then((result)=> {
+        console.log(result);
+        window.contract.methods.buy().send({from:this.$store.state.web3.coinbase, gas:result,value:this.recvAmount}).then((res)=>{
+          console.log(res);
+        })
+      }); 
+    },
+    copyAddress:function() {
+      var copyText = document.getElementById("address");
+      copyText.select();
+      copyText.setSelectionRange(0, 99999);
+      document.execCommand("copy");   
+      document.getSelection().removeAllRanges();
+      this.copiedTooltip = true;
+      setTimeout(() => {
+        this.copiedTooltip=false;
+      }, 1000);
+    },
     padNum: function (num) {
       var zero = '';
       for (var i = 0; i < 2; i++) {
@@ -450,7 +565,7 @@ export default {
           await window.ethereum.request({ method: 'eth_requestAccounts' }).then((result)=>{
             console.log("result="+result)
             document.location.reload();
-          });          
+          });
         }else {
           console.log("here",this.$store.state.web3.coinbase)
           let params = [
@@ -474,18 +589,15 @@ export default {
     }
   }
 }
-
-function copyToClipboard() {
-  var copyText = document.getElementById("address");
-
-  /* Select the text field */
-  copyText.select();
-  copyText.setSelectionRange(0, 99999); /* For mobile devices */
-
-  /* Copy the text inside the text field */
-  document.execCommand("copy");
-
-  /* Alert the copied text */
-  alert("Copied the text: " + copyText.value);
-}
 </script>
+<style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
